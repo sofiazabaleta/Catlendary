a11ec28bd54f22f14610280e86a8494ae266e860
Ariadna Zabaleta <ariadnazabaleta@Ariadnas-MacBook-Pro.local>
2023-06-20 21:01:01 -0400

    added calendar render and functionality

--- src/pages/Catlendary.jsx
+++ src/pages/Catlendary.jsx
@@ -1,10 +1,84 @@
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
+import { format, getDay, parse, startOfWeek } from "date-fns";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import DatePicker from "react-datepicker";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+import { enUS } from "date-fns/locale";
+import { useState } from "react";
+import { Stack } from "@mantine/core";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "play league of legends",
+    start: new Date(),
+    end: new Date(),
+  },
+];
 
 export default function Catlendary() {
-  return <h1>Catlendary</h1>;
+  const [newEvent, setNewEvent] = useState({
+    title: "",
+    start: new Date(),
+    end: new Date(),
+  });
+
+  const [allEvents, setAllEvents] = useState(events);
+
+  function handleAddEvent() {
+    setAllEvents([...allEvents, newEvent]);
+  }
+
+  return (
+    <>
+      <h1>Catlendary</h1>
+      <h2>Add New Event</h2>
+      <div style={{ position: "relative", zIndex: 100 }}>
+        <input
+          type="text"
+          placeholder="Add title"
+          style={{ width: "20%", marginRight: "10px" }}
+          value={newEvent.title}
+          onChange={(e) =>
+            setNewEvent((prev) => ({ ...prev, title: e.target.value }))
+          }
+        />
+        <div>
+          <DatePicker
+            placeholderText="Start Date"
+            selected={newEvent.start}
+            onChange={(date) =>
+              setNewEvent((prev) => ({ ...prev, start: date }))
+            }
+          />
+          <DatePicker
+            placeholderText="End Date"
+            selected={newEvent.end}
+            onChange={(date) => setNewEvent((prev) => ({ ...prev, end: date }))}
+          />
+        </div>
+        <button style={{ marginTop: "10px" }} onClick={handleAddEvent}>
+          Add Event
+        </button>
+      </div>
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "58px" }}
+      />
+    </>
+  );
 }
